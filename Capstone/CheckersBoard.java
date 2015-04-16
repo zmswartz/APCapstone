286d6f2c18c3f6443e006f649aee0dcb417254fa
zmswartz <zmswartz@NN123-HA-22.HS.sd203.org>
2015-04-16 12:29:42 -0500

    Capstone

--- Capstone/CheckersBoard.java
+++ Capstone/CheckersBoard.java
@@ -1,6 +1,6 @@
-
+import java.util.ArrayList;
 /**
- * Write a description of class CheckersBoard here.
+ * Write a description of class CheckersBoard here
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -62,10 +62,8 @@ public class CheckersBoard
         {
             game[(y+oldY)/2][(x+oldX)/2] = null;
         }
-        
-        
-    }
 
+    }
     /**
      * An example of a method - replace this comment with your own
      *  that describes the operation of the method
@@ -82,7 +80,7 @@ public class CheckersBoard
         // put your code here
         return false;
     }
-    
+
     /**
      * An example of a method - replace this comment with your own
      *  that describes the operation of the method
@@ -94,10 +92,48 @@ public class CheckersBoard
      * @param   y   description of parameter y
      * @return  description of the return value
      */
-    public String forcedMove(int player)
+    public String[] forcedMove(int player)
     {
         // put your code here
-        return "a";
+        ArrayList<String> forced = new ArrayList<String>();
+        for(int y = 0 ; y < game.length; y++)
+        {
+            for(int x = 0; x < game[0].length; x++)
+            {
+                Piece temp = getPieceAt(x,y);
+                if(temp!= null && temp.getTeam() == player)
+                {
+                    int[][] directions = temp.getMoveDirections();
+                    for(int i = 0; i < directions.length; i++)
+                    {
+
+                        int newX = x + directions[i][0];
+                        int newY = y + directions[i][1];
+                        if(newX > 0 && newX < 7 && newY > 0 && newY < 7 && getPieceAt(newX, newY) !=null)
+                        {
+                            int newX2 = newX + directions[i][0];
+                            int newY2 = newY + directions[i][1];
+                            if(newX2 > 0 && newX2 < 7 && newY2 > 0 && newY2 < 7 && getPieceAt(newX2, newY2) ==null)
+                            {
+                                String thisMove = "";
+                                thisMove += x;
+                                thisMove += y;
+                                thisMove += (newX2);
+                                thisMove += (newY2);
+                                forced.add(thisMove);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        String[] forcedArray = new String[forced.size()];
+        for(int i = 0; i < forcedArray.length; i++)
+        {
+            forcedArray[i] = forced.get(i);
+        }
+        return forcedArray;
     }
 
     /**
@@ -140,7 +176,7 @@ public class CheckersBoard
             }
         }
     }
-    
+
     /**
      * An example of a method - replace this comment with your own
      *  that describes the operation of the method
@@ -158,28 +194,32 @@ public class CheckersBoard
         Piece temp = getPieceAt(x,y);
         int[][] directions = temp.getMoveDirections();
         String[] moves = new String[directions.length];
+        int movesIndex = 0;
         for(int i = 0; i < directions.length; i++)
         {
-                if(x+directions[i][0] < 0 || x+directions[i][0] > 7 || y+directions[i][1] < 0 
-                                            || y+directions[i][1] > 7)
-                {
-                    String[] moves2 = new String[moves.length -1];
-                    for(int z = 0; z < moves.length-1; z++)
-                        moves2[z] = moves[z];
-                    moves = moves2;
-                }
+            if(x+directions[i][0] < 0 || x+directions[i][0] > 7 || y+directions[i][1] < 0 
+            || y+directions[i][1] > 7 || getPieceAt(x+directions[i][0],y+directions[i][1]) != null )
+            {
+                String[] moves2 = new String[moves.length -1];
+                for(int z = 0; z < moves.length-1; z++)
+                    moves2[z] = moves[z];
+                moves = moves2;
+            }
+            else
+            {
                 String thisMove = "";
                 thisMove += x;
                 thisMove += y;
                 thisMove += (x+directions[i][0]);
                 thisMove += (y+directions[i][1]);
-                moves[i] = thisMove;
+                moves[movesIndex] = thisMove;
+                movesIndex++;
+            }
         }
-        
+
         return moves;
     }
 
-    
     
     public void printer()
     {
@@ -188,9 +228,9 @@ public class CheckersBoard
             for (int j = 0; j <8 ; j++)
             {
                 if( game[i][j] != null)
-                System.out.print(game[i][j].getTeam() + "\t");
+                    System.out.print(game[i][j].getTeam() + "\t");
                 else
-                System.out.print("\t");
+                    System.out.print("\t");
             }
             System.out.println();
         }
--- Capstone/Tester.java
+++ Capstone/Tester.java
@@ -7,15 +7,17 @@ public class Tester
         board.printer();
         
         System.out.println("\n");
-        System.out.println(Arrays.toString(board.getMovesOfPiece(1,2)));
-//         board.makeMove("1223");
-//         System.out.println("\n");
-//         board.printer();
-//         
-//         board.makeMove("0514");
-//         System.out.println("\n");
-//         board.printer();
-//         
+        
+        
+        board.makeMove("1203");
+        
+        
+        board.makeMove("0314");
+        System.out.println("\n");
+        board.printer();
+        board.forcedMove(-1);
+        
+        
 //         board.makeMove("2305");
 //         System.out.println("\n");
 //         board.printer();
